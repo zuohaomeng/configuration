f0dbb002aca4e6bc6393b6f9e52e56c0c5e90383
zuohaomeng <178183852@qq.com>
2020-04-03 21:23:56 +0800

    security

---
+++ security/src/test/java/com/meng/configuration/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meng.configuration.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
